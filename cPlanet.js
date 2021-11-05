705d6ba88e9241320d0c012bfb845be7c95e4619
Stijn <stijn.timmerman@student.nhlstenden.com>
2021-11-05 13:55:36 +0100

    GH pages update

--- cPlanet.js
+++ cPlanet.js
@@ -1,5 +1,6 @@
-import { lerp } from './node_modules/three/src/math/MathUtils.js'
-import * as THREE from './node_modules/three/build/three.module.js';
+import { lerp } from '../node_modules/three/src/math/MathUtils.js'
+import * as THREE from '../node_modules/three/build/three.module.js';
+
 
 
 
